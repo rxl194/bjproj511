ae970752ee31b522e22cefef6f9645b7c88b9c21
rxl194 <Rendong_Lin@yahoo.com>
2015-12-19 13:43:23 -0800

    Signed-off-by: rxl194 <Rendong_Lin@yahoo.com>

---
+++ server.js
@@ -0,0 +1,9 @@
+
+var express = require('./config/express');
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+app.listen(PORT);
+module.exports = app;
+
+console.log('Server running at port ' + PORT + '!');
