b2684972ec4b8c1d0cc3683a7c6d45a140944f27
rxl194 <Rendong_Lin@yahoo.com>
2015-12-21 22:35:44 -0500

    mongoose

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var express = require('./config/express');
+var mongoose = require('./config/mongoose'),
+  express = require('./config/express');
+
 var PORT = process.env.PORT || 3000;
 
+var db = mongoose();
 var app = express();
 app.listen(PORT);
 module.exports = app;
